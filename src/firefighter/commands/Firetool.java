52e90de9ad68c1cd44ab24e4527a9b58e7c5ac49
Stijn Bannink <stijnbannink13@gmail.com>
2019-07-13 11:24:24 +0200

    Beautified the code

--- src/firefighter/commands/Firetool.java
+++ src/firefighter/commands/Firetool.java
@@ -17,42 +17,42 @@ import firefighter.utility.HotbarMessager;
 import firefighter.utility.XMaterial;
 
 public class Firetool implements CommandExecutor {
-	private Main mainClass;
-	public Firetool(Main mainClass) {
-		this.mainClass = mainClass;
-	}
-	
-	@Override
-	public boolean onCommand(CommandSender Sender, Command cmd, String label, String[] args) {
-		if (!(Sender instanceof Player)) {
-			Sender.sendMessage(mainClass.prefix + "Only players can execute this command!");
-			return true;
-		}
-		Player p = (Player)Sender;
-		if (!(p.hasPermission(mainClass.getPermission("firetool_get")) || p.isOp())) {
-			p.sendMessage(mainClass.messages.get("invalid_permissions"));
-			return true;
-		}
-		ItemStack fire_extinguisher = XMaterial.IRON_HOE.parseItem();
-		//getting meta
-		ItemMeta meta = fire_extinguisher.getItemMeta();
-		//modifying meta
-		meta.setDisplayName("§c§lFire Extinguisher");
-		List<String> lore = new ArrayList<String>();
-		lore.add("Fire Extinguisher");
-		lore.add("§e§n" + mainClass.messages.get("hold_right_click"));
-		meta.addItemFlags(ItemFlag.HIDE_DESTROYS);
-		meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
-		//setting meta
-		meta.setLore(lore);
-		fire_extinguisher.setItemMeta(meta);
-		Inventory inventory = p.getInventory();
-		inventory.addItem(fire_extinguisher);
-		try {
-			HotbarMessager.sendHotBarMessage(p, "§e§n" + mainClass.messages.get("hold_right_click"));
-		} catch (Exception e) {}
-		
-		return true;
-	}
+    private Main mainClass;
+    public Firetool(Main mainClass) {
+        this.mainClass = mainClass;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender Sender, Command cmd, String label, String[] args) {
+        if (!(Sender instanceof Player)) {
+            Sender.sendMessage(mainClass.prefix + "Only players can execute this command!");
+            return true;
+        }
+        Player p = (Player) Sender;
+        if (!(p.hasPermission(mainClass.getPermission("firetool_get")) || p.isOp())) {
+            p.sendMessage(mainClass.messages.get("invalid_permissions"));
+            return true;
+        }
+        ItemStack fire_extinguisher = XMaterial.IRON_HOE.parseItem();
+        //getting meta
+        ItemMeta meta = fire_extinguisher.getItemMeta();
+        //modifying meta
+        meta.setDisplayName("§c§lFire Extinguisher");
+        List < String > lore = new ArrayList < String > ();
+        lore.add("Fire Extinguisher");
+        lore.add("§e§n" + mainClass.messages.get("hold_right_click"));
+        meta.addItemFlags(ItemFlag.HIDE_DESTROYS);
+        meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+        //setting meta
+        meta.setLore(lore);
+        fire_extinguisher.setItemMeta(meta);
+        Inventory inventory = p.getInventory();
+        inventory.addItem(fire_extinguisher);
+        try {
+            HotbarMessager.sendHotBarMessage(p, "§e§n" + mainClass.messages.get("hold_right_click"));
+        } catch (Exception e) {}
+
+        return true;
+    }
 
 }
